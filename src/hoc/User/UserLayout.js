b16f636bf0778c2f3da8431ee09e77b9fa0cf8af
Erayus <raymondhieutran@gmail.com>
2019-11-17 23:07:59 +1100

    [New] Add selected school to the local storage

--- src/hoc/User/UserLayout.js
+++ src/hoc/User/UserLayout.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react';
 import classes from "./UserLayout.module.css";
 
 class UserLayout extends Component {
+
+    onSelectSchool(event){
+        localStorage.setItem('selectedSchool', event.target.value )
+    }
+
     render(){
         return (
             <div className={classes.UserLayout}>
                 <div className={classes.schoolSelection}>
                     <h1>Select a school</h1>
-                    <select>
-                        <option>Selection</option>
-                        <option>St Margaret Primary School</option>
+                    <select onChange={this.onSelectSchool}>
+                        <option value="none">Selection</option>
+                        <option value="St Margaret Primary School">St Margaret Primary School</option>
+                        <option value="Gardenvale Primary School">Gardenvale Primary School</option>
+                        <option value="Geelong Grammar Primary School">Gardenvale Primary School</option>
                     </select>
                 </div>
             </div>
