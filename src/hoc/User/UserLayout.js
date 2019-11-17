03f5e93d8ddae95e5bc2e8732014f6870b099825
Erayus <raymondhieutran@gmail.com>
2019-11-17 22:59:51 +1100

    [New] Added School Selection box

--- src/hoc/User/UserLayout.js
+++ src/hoc/User/UserLayout.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
-
+import classes from "./UserLayout.module.css";
 
 class UserLayout extends Component {
     render(){
         return (
-            <div>
-                User Layout
+            <div className={classes.UserLayout}>
+                <div className={classes.schoolSelection}>
+                    <h1>Select a school</h1>
+                    <select>
+                        <option>Selection</option>
+                        <option>St Margaret Primary School</option>
+                    </select>
+                </div>
             </div>
         )
     }
