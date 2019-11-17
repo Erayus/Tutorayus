4ae6f01ed1336ef503f6c1c1bf08764509a76559
Erayus <raymondhieutran@gmail.com>
2019-11-17 23:30:45 +1100

    [New] Cache the selected school and display it upon user arrival

--- src/hoc/User/UserLayout.js
+++ src/hoc/User/UserLayout.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import classes from "./UserLayout.module.css";
 
 class UserLayout extends Component {
+    constructor(props) {
+        super(props);
+        this.schoolSelectionBox= React.createRef();
+      }
+
+    componentDidMount(){
+        const selectedSchool = localStorage.getItem('selectedSchool');
+        this.schoolSelectionBox.current.value = selectedSchool;
+    }
 
     onSelectSchool(event){
         localStorage.setItem('selectedSchool', event.target.value )
@@ -12,11 +21,11 @@ class UserLayout extends Component {
             <div className={classes.UserLayout}>
                 <div className={classes.schoolSelection}>
                     <h1>Select a school</h1>
-                    <select onChange={this.onSelectSchool}>
+                    <select onChange={this.onSelectSchool} ref={this.schoolSelectionBox}>
                         <option value="none">Selection</option>
                         <option value="St Margaret Primary School">St Margaret Primary School</option>
                         <option value="Gardenvale Primary School">Gardenvale Primary School</option>
-                        <option value="Geelong Grammar Primary School">Gardenvale Primary School</option>
+                        <option value="Geelong Grammar Primary School">Geelong Grammar Primary School</option>
                     </select>
                 </div>
             </div>
