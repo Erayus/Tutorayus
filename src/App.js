71e0ea5c411a09ef3db8d1e98c26effffb3adde7
Erayus <raymondhieutran@gmail.com>
2019-11-19 10:35:38 +1100

    [New] Redirect / to /user

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Route, Switch} from 'react-router-dom' ;
+import {Route, Switch, withRouter} from 'react-router-dom' ;
 import AdminLayout from "./hoc/Admin/AdminLayout";
 import UserLayout from "./hoc/User/UserLayout";
 
-function App() {
-  return (
-    <div className="App">
-      <Route path='/user' component={UserLayout}/>
-      <Route path= '/admin' component={AdminLayout}/>
-    </div>
-  );
+class App extends Component {
+  componentDidMount = () =>{
+    console.log(this.props);
+    if (this.props.location.pathname == '/'){
+      this.props.history.push('user')
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <Route path='/user' component={UserLayout}/>
+        <Route path= '/admin' exact component={AdminLayout}/>
+      </div>
+    );
+  }
+ 
 }
 
-export default App;
+export default withRouter(App);
