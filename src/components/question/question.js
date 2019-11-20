50e089cda36a63f4aa0215170a9847700a318069
Erayus <raymondhieutran@gmail.com>
2019-11-20 21:33:40 +1100

    [New] Displaying question and answering questions mechanism

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,19 +1,26 @@
 import {MDBBtn} from 'mdbreact';
 import React from 'react';
 const question = (props) => {
+    const questionAnswered = (response) => {
+        props.responsed(props.id, response)
+    }
+
     let questionBody = null;
     switch(props.type) {
         case 'yesno':
             questionBody = (
                 <React.Fragment>
-                    <MDBBtn color="danger">No</MDBBtn>
-                    <MDBBtn color="success">Yes</MDBBtn>
+                    <MDBBtn color="danger" onClick={()=> questionAnswered('No')}>No</MDBBtn>
+                    <MDBBtn color="success" onClick={()=> questionAnswered('Yes')}>Yes</MDBBtn>
                 </React.Fragment>    
             );
             break;
         default:
             alert('Unsupported Question Type')
     }
+
+  
+
 
     return (
         <div>
--- src/containers/Survey/Survey.js
+++ src/containers/Survey/Survey.js
@@ -6,7 +6,25 @@ import classes from "./Survey.module.css";
 
 class Survey extends Component {
     state = {
-        surveyingSchool: null
+        surveyingSchool: null,
+        questions: [
+            {
+                id: "1",
+                title: 'Did you have fun today?',
+                type: 'yesno'
+            },
+            {
+                id: "2",
+                title: 'Did you learn something new today?',
+                type: 'yesno'
+            },
+            {
+                id: "3",
+                title: "Do you love today's challenges?",
+                type: 'yesno'
+            }
+        ],
+        curQuestionIndex: 0
     }
 
     componentDidMount = () => {
@@ -16,7 +34,21 @@ class Survey extends Component {
     backToSchoolSelection = () => {
         this.props.history.replace('/')
     }
+    onQuestionAnswered = (id, response) => {
+        console.log(id + ": " + response)
+        if (this.state.curQuestionIndex < this.state.questions.length - 1){
+            this.setState({curQuestionIndex: this.state.curQuestionIndex + 1})
+        }
+    }
+
     render(){
+    let displayingQuestion = <Question
+                                id={this.state.questions[this.state.curQuestionIndex].id}
+                                type={this.state.questions[this.state.curQuestionIndex].type}
+                                responsed={(id, response) => this.onQuestionAnswered(id, response)}
+                            >
+                                {this.state.questions[this.state.curQuestionIndex].title}
+                            </Question>
         return (
             <WhiteBox>
                 <div className={classes.SurveyHead}>
@@ -28,7 +60,7 @@ class Survey extends Component {
                             "top": "5px",
                             "position": "fixed",
                         }}
-                        onClick={this.backToSchoolSelection}  >
+                        onClick={this.backToSchoolSelection} >
                         <i className="fa fa-arrow-circle-o-left" style={{"marginRight": "5px", "fontSize": '18px'}}aria-hidden="true"></i> 
                         School Selection
                     </MDBBtn>
@@ -36,7 +68,7 @@ class Survey extends Component {
                 </div>
                 
                 <div className={classes.SurveyBody}>
-                    <Question type="yesno">Did you have fun today?</Question>    
+                    {displayingQuestion} 
                 </div>
             </WhiteBox>
         )  
