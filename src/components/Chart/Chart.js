9de04e5fd7eb13ab935cfc8b1c7bd15b8f1a2e6e
Erayus <raymondhieutran@gmail.com>
2019-12-03 02:37:44 +1100

    [New] Passed data to reports and display them

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -8,13 +8,13 @@ const chart = (props) =>{
           {
             data: [ props.noOfYes, props.noOfNo],
             backgroundColor: ['#00C851', '#e53935'],
-            hoverBackgroundColor: ['#b71c1c', '#1b5e20']
+            hoverBackgroundColor: ['#1b5e20', '#b71c1c']
           }
         ]
       };
     return (
         <div style={{width: 350}}>
-            <h5>{props.questionTitle}</h5>
+            <h5>{props.title}</h5>
             <Chart  type='doughnut' data={data} />   
         </div>
     )
--- src/containers/Report/Report.js
+++ src/containers/Report/Report.js
@@ -1,22 +1,51 @@
 import React, {Component} from 'react';
 import axios from '../../axios-survey';
+import Chart from '../../components/Chart/Chart';
 
 class Report extends Component {
     state = {
-        schoolName: null
+        schoolName: null,
+        questions: null,
+        questionTitles: {
+            'Q001': 'Did You Have Fun Today?',
+            'Q002': 'Did You Learn Something New Today?',
+            'Q003': 'Do You Love Today\'s Chanllenges?' 
+        }
     }
     componentDidMount(){
         const schoolName = this.props.match.params.schoolName;
-        this.setState({schoolName: schoolName})   
+        this.setState({schoolName: schoolName});  
         axios.get('/surveys/' + schoolName + '.json').then(
-            (res)=> console.log(res.data)
+            (res)=> {
+                console.log(res.data)
+                this.setState({questions: res.data})
+            }
         )
     }
     render(){
+        let questionData = [];
+        for (let questionNum in this.state.questions){
+            let question = {};
+            question['title'] = this.state.questionTitles[questionNum];
+            question['noOfYes'] = this.state.questions[questionNum]['Yes'];
+            question['noOfNo'] = this.state.questions[questionNum]['No'];
+            questionData.push(question)
+        }
+        let charts = (questionData.map(question => {
+            return <Chart 
+                        key={question.title} 
+                        title={question.title} 
+                        noOfYes={question.noOfYes} 
+                        noOfNo={question.noOfNo}/>
+        }))
+
         return(
-            <div>
-                <h3><strong>{this.state.schoolName}</strong></h3>
-            </div>
+            <React.Fragment>
+                <h2 style={{marginTop: '20px'}}><strong>{this.state.schoolName}</strong></h2>
+                <div style={{display: 'flex', justifyContent: 'center' ,marginTop: '80px'}}>
+                    {charts}
+                </div>
+            </React.Fragment>
         )
     }
 }
